f647a157ed1cafac5fefd67ea5261d50d3bd4aa2
xielidawan <54834260+xielidawan@users.noreply.github.com>
2020-09-23 22:55:47 -0700

    Delete random_number.py

--- random_number.py
+++
@@ -1,9 +0,0 @@
-import random
-import json
-
-num_list = []
-for i in range(1000):
-    num_list.append(random.randint(0, 100))
-
-with open('file1.json', 'w') as f:
-    f.write(json.dumps(num_list))
